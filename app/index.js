a9b777899fd07e1a971f5795dc906996a7f40fbd
James Blackburn <blackburnfjames@gmail.com>
2022-05-31 17:51:07 +0100

    Added full fitfont support for text

--- app/index.js
+++ app/index.js
@@ -2,12 +2,31 @@ import clock from "clock";
 import * as document from "document";
 import { preferences } from "user-settings";
 import * as util from "../common/utils";
+import { FitFont } from 'fitfont'
 
 // Update the clock every minute
 clock.granularity = "minutes";
 
 // Get a handle on the <text> element
-const myLabel = document.getElementById("myLabel");
+// const myLabel = document.getElementById("myLabel");
+
+// Get fitfont label
+const timeLabel = new FitFont({ 
+  id:'timeLabel',               // id of your symbol in the index.gui, you can also give an element object e.g. id: document.getElementById('foo')
+  font:'Roboto_Mono_16', // name of the generated font folder
+
+  // Optional
+  halign: 'start',            // horizontal alignment : start / middle / end
+  valign: 'baseline',         // vertical alignment   : baseline / top / middle / bottom
+  letterspacing: 0            // letterspacing...
+})
+
+const basicCommand = new FitFont({ 
+  id:'basicCommand',               // id of your symbol in the index.gui, you can also give an element object e.g. id: document.getElementById('foo')
+  font:'Roboto_Mono_16', // name of the generated font folder
+})
+
+basicCommand.text = "A> BASIC.COM";
 
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
@@ -21,5 +40,5 @@ clock.ontick = (evt) => {
     hours = util.zeroPad(hours);
   }
   let mins = util.zeroPad(today.getMinutes());
-  myLabel.text = `${hours}:${mins}`;
+  timeLabel.text = `${hours}:${mins}`;
 }
